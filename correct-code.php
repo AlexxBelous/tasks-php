f0208e0c2a84bfcd185f81d528b94226a39cce75
AlexxBelous <connectionwithme1@gmail.com>
2024-09-17 07:09:04 +0300

    add new task 5

--- correct-code.php
+++ correct-code.php
@@ -22,16 +22,39 @@
 
 
 
-<ul>
-    <?php foreach ($users as $user) : ?>
-        <li style="margin-bottom: 25px; list-style: none">
-            <strong>Name:</strong> <?php echo $user['name']; ?><br>
-            <strong>Email:</strong> <?php echo $user['email']; ?><br>
-            <?php if (array_key_exists('age', $user)) : ?>
-                <strong>Age:</strong> <?php echo $user['age']; ?><br>
-            <?php else : ?>
-                <em>Age not found</em><br>
-            <?php endif; ?>
-        </li>
-    <?php endforeach; ?>
-</ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+$string = implode(' ', $array);
+?>
