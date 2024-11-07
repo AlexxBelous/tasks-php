0768a4bfcfa74e8c1373f1a925878561cf8d3322
AlexxBelous <connectionwithme1@gmail.com>
2024-11-07 06:52:18 +0200

    add new task 12

--- index.php
+++ index.php
@@ -1,29 +1,15 @@
 <?php
-namespace App\Classes;
-
 
 require_once __DIR__ . '/src/App/Functions/functions.php';
 require_once __DIR__ . '/vendor/autoload.php';
 //path database.php - src/App/Database/database.php
 
-$getString = require('src/App/Database/database.php');
+$posts = require('src/App/Database/database.php');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$newPost = [
+    "id" => 30,
+    "title" => "My test title for slug",
+    "slug" => "my-test-title-for-slug",
+    "excerpt" => "A look into the world of subatomic particles.",
+    "content" => "This field challenges our understanding of reality, revealing that particles can exist in multiple states at once. Quantum physics explores the behavior of particles at the smallest scales."
+];
